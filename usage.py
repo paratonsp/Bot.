05dfb2e1b6e225743403a92d0fd04ba89d344a46
Paraton Suryo Prayogo <70687618+paratonsp@users.noreply.github.com>
2023-05-14 01:02:19 +0700

    update

--- usage.py
+++
@@ -1,9 +0,0 @@
-import psutil
-import time
-
-while True:
-    print("----------------")
-    print(psutil.cpu_percent())
-    print(psutil.virtual_memory().percent)
-    print("----------------")
-    time.sleep(1)
