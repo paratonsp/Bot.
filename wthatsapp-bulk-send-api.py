c14b36037ee8d5fcd8a6106407ca4cd29484edc7
Paraton Suryo Prayogo <70687618+paratonsp@users.noreply.github.com>
2023-06-30 05:22:59 +0700

    update

--- wthatsapp-bulk-send-api.py
+++ wthatsapp-bulk-send-api.py
@@ -6,8 +6,6 @@ count = 0
 
 excel_data = pandas.read_excel('List Data.xlsx', sheet_name='Data')
 API_ENDPOINT_SEND = "http://103.183.75.212:3000/api/sendText"
-API_ENDPOINT_START = "http://103.183.75.212:3000/api/startTyping"
-API_ENDPOINT_STOP = "http://103.183.75.212:3000/api/stopTyping"
 
 for column in excel_data['Name'].tolist():
     message = excel_data['Message'][0]
@@ -16,11 +14,7 @@ for column in excel_data['Name'].tolist():
     contact = person + '@c.us'
 
     data = {'chatId': contact, 'text': message, 'session': 'default'}
-    dataTyping = {'chatId': contact, 'session': 'default'}
 
-    requests.post(url=API_ENDPOINT_START, data=dataTyping)
-    time.sleep(3)
-    requests.post(url=API_ENDPOINT_STOP, data=dataTyping)
     res = requests.post(url=API_ENDPOINT_SEND, data=data)
     print('STATUS: ' + str(res.status_code) + ' | Send to ' + column)
 
