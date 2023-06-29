b9ccaf1c5b57941d1045b4f427e2d5b713fbae3b
Paraton Suryo Prayogo <70687618+paratonsp@users.noreply.github.com>
2023-06-30 04:46:47 +0700

    Avoid Blocking

--- wthatsapp-bulk-send-api.py
+++ wthatsapp-bulk-send-api.py
@@ -1,25 +1,31 @@
 import time
 import requests
 import pandas
+import random
 
 count = 0
 
 excel_data = pandas.read_excel('List Data.xlsx', sheet_name='Data')
-API_ENDPOINT = "http://103.183.75.212:3000/api/sendText"
+API_ENDPOINT_SEND = "http://103.183.75.212:3000/api/sendText"
+API_ENDPOINT_START = "http://103.183.75.212:3000/api/startTyping"
+API_ENDPOINT_STOP = "http://103.183.75.212:3000/api/stopTyping"
 
 for column in excel_data['Name'].tolist():
-    # init
     message = excel_data['Message'][0]
     person = str(excel_data['Contact'][count])
     message = message.replace('{customer_name}', column)
     contact = person + '@c.us'
 
     data = {'chatId': contact, 'text': message, 'session': 'default'}
+    dataTyping = {'chatId': contact, 'session': 'default'}
 
-    res = requests.post(url=API_ENDPOINT, data=data)
+    res = requests.post(url=API_ENDPOINT_START, data=dataTyping)
+    time.sleep(random.randint(1, 5))
+    res = requests.post(url=API_ENDPOINT_STOP, data=dataTyping)
+    res = requests.post(url=API_ENDPOINT_SEND, data=data)
     print('STATUS: ' + str(res.status_code) + ' | Send to ' + column)
 
     count = count + 1
-    time.sleep(3)
+    time.sleep(5)
 
 print('TOTAL: ' + str(count))
